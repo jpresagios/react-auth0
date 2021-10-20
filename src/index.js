a62696748dc4408a48bd0df275101f7fc9621fe9
Junior Paz Formoso <junior.formoso@sap.com>
2021-10-20 15:36:42 +0300

    Settings everything to allow authentication

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Auth0Provider } from "./contexts/auth0-context";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Auth0Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
