24789c4a00c3b242e8156b486ab1f0305b2df58c
Junior Paz Formoso <junior.formoso@sap.com>
2021-10-20 14:48:39 +0300

    Added initial config

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "bulma/css/bulma.css";
+
+function App() {
+  return (
+    <div className="hero is-info is-fullheight">
+      <div className="hero-body">
+        <div className="container has-text-centered">my app goes here!</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
